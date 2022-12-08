a1ebeb7cf96e6cd9ed7b9bb8f6775dbeed68b289
André Johansson <andre@rduce.net>
2022-12-08 16:09:46 +0100

    feat: Add GA4 analytics
    Will only be active in production builds.
    
    See
    https://docusaurus.io/docs/api/plugins/@docusaurus/plugin-google-gtag

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -67,6 +67,10 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        gtag: {
+          trackingID: 'G-9M1JWJLRQL',
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
