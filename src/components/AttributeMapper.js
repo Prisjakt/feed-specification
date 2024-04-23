d771ffb2300addc33f7d40b9fe932144172a149c
André Johansson <andre.johansson@prisjakt.nu>
2024-04-23 08:44:21 +0200

    feat: Wrap attributemappre in BrowserOnly, does not support static site generation

--- src/components/AttributeMapper.js
+++ src/components/AttributeMapper.js
@@ -3,6 +3,7 @@ import AttributeTreeView from '@site/src/components/AttributeTreeView';
 import AttributeMapperPreview from '@site/src/components/AttributeMapperPreview';
 import attributeData from "./attribute-data.json";
 import { AttributeMapperContext, AttributeMapperDataContext } from "./AttributeMapperContext";
+import BrowserOnly from '@docusaurus/BrowserOnly';
 
 /* Set parent for each child */
 const setParent = (o) => {
@@ -19,15 +20,17 @@ const setParent = (o) => {
 export default function AttributeMapper() {
     const [selectedAttributes, setSelectedAttributes] = React.useState([]);
     const [data, setData] = React.useState(setParent({ children: attributeData }).children);
-    return (<AttributeMapperDataContext.Provider value={{ data, setData }}>
-        <AttributeMapperContext.Provider value={{ selectedAttributes, setSelectedAttributes }}>
-            <div className="column left">
-                <AttributeTreeView data={data} />
-            </div>
+    return (
+        <BrowserOnly fallback={<div>Loading...</div>}>
+            <AttributeMapperDataContext.Provider value={{ data, setData }}>
+                <AttributeMapperContext.Provider value={{ selectedAttributes, setSelectedAttributes }}>
+                    <div className="column left">
+                        <AttributeTreeView data={data} />
+                    </div>
 
-            <div className="column right">
-                <AttributeMapperPreview />
-            </div>
-        </AttributeMapperContext.Provider>
-    </AttributeMapperDataContext.Provider>);
+                    <div className="column right">
+                        <AttributeMapperPreview />
+                    </div>
+                </AttributeMapperContext.Provider>
+            </AttributeMapperDataContext.Provider>  </BrowserOnly>);
 }
