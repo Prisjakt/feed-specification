c486f7cb9e714caebc2ceb23b45777d8054ba3ac
André Johansson <andre.johansson@prisjakt.nu>
2024-04-26 10:13:19 +0200

    feat: Update generated attributes data

--- src/components/AttributeTreeView.js
+++ src/components/AttributeTreeView.js
@@ -16,7 +16,7 @@ import {
 import attributeData from '@site/static/attribute-data.json';
 
 const CustomTreeLabel = ({node, selectedAttributes, setSelectedAttributes, ...props}) => {
-	const isAttribute = node?.isAttribute === true;
+	const isAttribute = node?.is_attribute === true;
 
 	const value = useMemo(
 		() => isAttribute && selectedAttributes.find((x) => x.id === node.id) !== undefined,
